6313228fb812b8a64ffe2d0a1bb7e9da92124da2
Kevin Magombe <kevinmagombe@outlook.com>
2026-09-01 15:44:30 +0100

    Move construction/assignment and std::move

---
+++ chapter_twentytwo/move_only_resource_handle/main.cpp
@@ -0,0 +1,58 @@
+#include <print>
+
+class ResourceHandle
+{
+public:
+    ResourceHandle(int val)
+    {
+        m_res = new int{val};
+    }
+
+    ~ResourceHandle()
+    {
+        delete m_res;
+    }
+
+    ResourceHandle(const ResourceHandle&) = delete; // disable copy constructor
+    ResourceHandle& operator=(const ResourceHandle&) = delete; // disable copy assignment
+
+    ResourceHandle(ResourceHandle&& other)
+    {
+        m_res = other.m_res;
+        other.m_res = nullptr;
+    }
+
+    ResourceHandle& operator=(ResourceHandle&& other)
+    {
+        if (&other == this)
+            return *this;
+        
+        delete m_res;
+        m_res = other.m_res;
+        other.m_res = nullptr;
+        return *this;
+    }
+
+    auto get() const {return *m_res;}
+
+
+private:
+    int* m_res;
+};
+
+int main()
+{
+    ResourceHandle handle1{1};
+    ResourceHandle handle2{2};
+    ResourceHandle handle3{std::move(handle2)};
+    ResourceHandle handle4{4};
+
+    std::println("Handle1: {}", handle1.get());
+    std::println("Handle3: {}", handle3.get());
+    std::println("Handle4: {}", handle4.get());
+
+    handle4 = std::move(handle1);
+    std::println("Handle4: {}", handle4.get());
+
+    return 0;
+}
